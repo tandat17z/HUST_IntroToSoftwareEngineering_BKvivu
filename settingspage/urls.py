e69442f0e56f9017403f98d0d14bf3c6f68acea9
pbtruong03 <tphanba@gmail.com>
2023-11-28 09:02:25 +0700

    add model

--- settingspage/urls.py
+++ settingspage/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'settingspage'
 urlpatterns = [
@@ -8,4 +9,4 @@ urlpatterns = [
     path('uploadavatar/', views.uploadAvatar, name='uploadAvatar'),
     path('loading', views.changeAvatar, name= "changeAvatar" ),
 
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_URL)
