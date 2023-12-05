94931418125a6250f1f0677d3b9d59b70ae82df1
ntd237 <nguyentiendung23072003@gmail.com>
2023-12-05 09:12:05 +0700

    add postpage

--- postspage/urls.py
+++ postspage/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from . import views
 
+# 3/12
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'postspage'
 urlpatterns = [
     path('', views.postsPage, name='postsPage'),
     
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_URL) # 3/12
