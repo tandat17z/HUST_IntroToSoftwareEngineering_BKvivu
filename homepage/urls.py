55f31efb65792c887509f4978c56ff852973e84a
ntd237 <nguyentiendung23072003@gmail.com>
2023-12-16 22:54:46 +0700

    update postpage

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
 from . import views
 
-
 app_name = 'homepage'
 urlpatterns = [
     path('', views.homePage, name='homePage'),
     path('login/', views.loginPage, name='loginPage'),
     path('login/register/', views.registerPage, name='registerPage'),
     path('test', views.test),
-    # path('')
 ]
--- postspage/views.py
+++ postspage/views.py
@@ -11,9 +11,28 @@ from homepage import models
 
 from homepage.models import Post # them mo hinh Post o homepage.models
 # from homepage.models import Sharer
+from homepage.models import Manager
+
+
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+
+def like_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    # Tăng số lượt thích của bài viết
+    post.like += 1
+    post.save()
+
+    # Trả về dữ liệu dưới dạng JSON
+    data = {'like_count': post.like}
+    return JsonResponse(data)
+
+
 
 # Create your views here.
 def postsPage(request):
     posts = Post.objects.filter().order_by('time')
+    managers = Manager.objects.filter().order_by('-avgStar')
     
-    return render(request, 'index.html', {'posts' : posts})
+    return render(request, 'index.html', {'posts' : posts, 'managers' : managers})
